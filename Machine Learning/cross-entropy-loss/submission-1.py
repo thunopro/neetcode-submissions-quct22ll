6308e1e4ae3077f2375c80ec13da36fdb258d0c7
Nguyễn Khắc Thụ <91926925+thunopro@users.noreply.github.com>
2026-04-11 04:22:45 +0700

    Add: cross-entropy-loss - submission-1

---
+++ Machine Learning/cross-entropy-loss/submission-1.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        eps = 1e-7 
+        y_pred = np.clip(y_pred,eps,1-eps)
+        loss = -np.mean(y_true*np.log(y_pred)+ (1-y_true) * np.log(1-y_pred)) 
+        return round(float(loss),4) 
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        eps = 1e-7 
+        y_pred = np.clip(y_pred,eps,1-eps) 
+        loss = -np.sum(y_true*np.log(y_pred))/y_true.shape[0]
+        return round(float(loss),4) 
